02cf17dd7da53dff41e50746c342042d847c57ed
Wendi Chen <epanistamai.proles@gmail.com>
2018-11-21 02:02:01 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -15,11 +15,9 @@
 def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
       return "I LOVE YOU TOO PUMPKIN!"
-  
   elsif phrase == phrase.upcase
       return "NO, NOT SINCE 1938!"
-  
-  else phrase != phrase.upcase
+  else 
       return "HUH?! SPEAK UP SONNY!"
   end
 end
